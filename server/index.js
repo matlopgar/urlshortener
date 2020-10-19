3e86f56a9569a94faf43db8640cf2975127bab07
Matheus Lopes <matheus.lopes@u4crypto.com>
2020-10-19 20:31:51 -0300

    public frontend

--- server/index.js
+++ server/index.js
@@ -9,12 +9,26 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
     res.json({
         message: 'codecast.sh - Encurtador de URL para o seu PodCast'
     });
 });
+
+app.get('/:id', (req, res) => {
+    //TODO: redirect to url
+});
+
+app.post('/url', (req, res) => {
+    //TODO: create a short url
+})
+
+app.get('/url/:id', (req, res) => {
+    //TODO: get a short url by id
+})
+
 
 
 const port = process.env.PORT || 8888;
